55d0b36b6ba6a37fa1143e0a8d824bfbfff591bf
Suchita Meshram <suchita.meshram@fractal.ai>
2023-02-26 23:28:11 +0530

    Script for pagination

---
+++ task_2.py
@@ -0,0 +1,24 @@
+from pip._vendor import requests
+import pprint
+
+header = {
+    "access_token": 'fe66583bfe5185048c66571293e0d358'
+}
+
+# parameters to fetch records
+params = {
+    "limit": 100,
+    "offset": 1
+}
+
+# empty list to store records
+all_records = [] 
+
+# loop to fetch 500 records
+while params.get("offset") < 500:
+    response = requests.get(f"https://globalmart-api.onrender.com/mentorskool/v1/sales", headers=header, params=params).json()
+
+    all_records.append(response)
+    params["offset"] = int(response.get("next").split("=")[1][:3])
+    pprint.pprint(all_records)
+    print(len(all_records))
